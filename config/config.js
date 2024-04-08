326c8817ca8edbe107bdd74fbdabe0209de6bf86
Sebastian <128631661+menita2005@users.noreply.github.com>
2024-04-07 20:44:20 -0500

    feat(backend): configurando servidor

---
+++ config/config.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+
+host: 'localhost',
+
+user: 'root',
+
+password: 'sebastian123',
+
+database: 'nodejs_base1'
+
+});
+
+db.connect(function(err) {
+
+if (err) throw err;
+
+console.log('Base de datos conectada');
+
+});
+
+module.exports = db;
---
+++ routes/userRoutes.js
@@ -0,0 +1,7 @@
+const usersController = require('../controllers/usersController');
+
+module.exports = (app) => {
+
+app.post('/api/users/create', usersController.register);
+
+}
